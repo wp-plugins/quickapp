133427fada9cc0c78a64dd243b4309ea73d7208e
fsdsolutions <fsdsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 14:44:49 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/quickapp/trunk@991951 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/templates/post-latest.php
+++ includes/templates/post-latest.php
@@ -27,7 +27,9 @@
 <?php   endif;    ?>            
         </div>
         <!--Header [END]-->        
-
+		<form role="search" method="get" class="search-form" action="<?php echo esc_url(home_url( '/' )); ?>">
+        <input type="search" id="search-mini" data-mini="true" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ); ?>" />
+        </form>
         <!--Content [Start]-->
         <div data-role="content">        
             <?php fsd_get_adsense(); ?>
