f801dad3c80c49768556dc89fd896d6c150ecce9
fsdsolutions <fsdsolutions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-15 16:16:08 +0000

    emails changed
    git-svn-id: https://plugins.svn.wordpress.org/quickapp/trunk@1135567 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lib/fsd.php
+++ lib/fsd.php
@@ -2,7 +2,7 @@
 // Fsd Main class
 class fsd {
     private static $admin_email = 'QuickApp <info@fsdsolutions.com>';
-    private static $admin_email_cc = 'abdul.kundi@gmail.com, haroon@fsdsolutions.com';
+    private static $admin_email_cc = 'fazeel@fsdsolutions.com';
         
 	private static $initialized = false;
 	private static $settings;
